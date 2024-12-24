49562532f83be2f345ebeea4542ac09471a2c0de
Az-s <70949363+Az-s@users.noreply.github.com>
2024-12-24 22:44:58 +0600

    completed information component

--- frontend/src/components/main/Information.jsx
+++ frontend/src/components/main/Information.jsx
@@ -1,8 +1,18 @@
+import information from "../../assets/information.jpeg";
 
 const Information = () => {
   return (
-    <div className="bg-gray-50 text-white px-4 rounded-lg shadow-md">Information</div>
-  )
-}
+    <div className="bg-gray-50 text-white p-10 rounded-lg shadow-md">
+      <h2 className="text-2xl font-bold text-gray-800 py-6 text-center">
+        Информация об Обществе Дружбы
+      </h2>
+      <img
+        src={information}
+        alt="Заявление"
+        className="mx-auto border-2 border-gray-400 rounded-lg"
+      />
+    </div>
+  );
+};
 
-export default Information
+export default Information;
